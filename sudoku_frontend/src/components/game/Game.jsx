d911b90b51e5f0feed21a3e92fa97a4541fb0364
Logan Thimer <thimerlogan@gmail.com>
2023-10-09 10:46:23 -0600

    Encapsulated more board functions inside Board component

--- sudoku_frontend/src/components/game/Game.jsx
+++ sudoku_frontend/src/components/game/Game.jsx
@@ -1,94 +1,20 @@
-import React, { useEffect, useState } from "react";
-import useKeypress from "react-use-keypress";
+import React, { useEffect, useRef, useState } from "react";
 import ActionBar from "../actionbar/ActionBar";
 import Board from "../board/Board";
 import "./Game.css";
-import { sameSquare, createBoard } from "../../util";
+import { createBoard } from "../../util";
 import GameSettings from "./GameSettings";
 import Congratulations from "./Congratulations";
 
 function Game() {
   const [board, setBoard] = useState([]);
   const [notes, setNotes] = useState(false);
-  const [selectedCell, setSelectedCell] = useState(null);
   const [gameComplete, setGameComplete] = useState(false);
   const [boardInitialized, setBoardInitialized] = useState(false);
   const [settingsConfirmed, setSettingsConfirmed] = useState(false);
   const [difficulty, setDifficulty] = useState("");
 
-  const setSelectedCellAsNum = (num) => {
-    const newBoard = [...board];
-
-    if (
-      selectedCell &&
-      board[selectedCell.row][selectedCell.col].value === 0 &&
-      board[selectedCell.row][selectedCell.col].editable
-    ) {
-      if (notes) {
-        if (board[selectedCell.row][selectedCell.col].notes.has(num)) {
-          board[selectedCell.row][selectedCell.col].notes.delete(num);
-        } else {
-          board[selectedCell.row][selectedCell.col].notes.add(num);
-        }
-      } else {
-        newBoard[selectedCell.row][selectedCell.col].value = num;
-        checkConflict(newBoard, newBoard[selectedCell.row][selectedCell.col]);
-      }
-
-      setBoard(newBoard);
-    }
-  };
-
-  const checkConflict = (brd, cell) => {
-    for (let i = 0; i < brd.length; i++) {
-      for (let j = 0; j < brd[i].length; j++) {
-        let cell = brd[i][j];
-
-        if (cell.value === 0) {
-          continue;
-        }
-
-        // initialize the conflict field to false for each item
-        cell.conflict = false;
-
-        for (let m = 0; m < brd.length; m++) {
-          for (let n = 0; n < brd[m].length; n++) {
-            let item = brd[m][n];
-
-            // check if the item is in the same row or column as the outer cell,
-            // has the same value, and is not the outer cell itself
-            if (
-              (item.row === cell.row ||
-                item.col === cell.col ||
-                sameSquare(cell.row, cell.col, item.row, item.col)) &&
-              item.value === cell.value &&
-              !(item.row === cell.row && item.col === cell.col)
-            ) {
-              cell.conflict = true;
-              item.conflict = true;
-            }
-          }
-        }
-      }
-    }
-  };
-
-  const clearSelectedCell = () => {
-    if (
-      selectedCell &&
-      board[selectedCell.row][selectedCell.col].value !== 0 &&
-      board[selectedCell.row][selectedCell.col].editable
-    ) {
-      const newBoard = [...board];
-      newBoard[selectedCell.row][selectedCell.col].value = 0;
-      checkConflict(newBoard);
-      setBoard(newBoard);
-    }
-  };
-
-  useKeypress(["1", "2", "3", "4", "5", "6", "7", "8", "9"], (event) => {
-    setSelectedCellAsNum(parseInt(event.key));
-  });
+  const boardRef = useRef();
 
   const settingsConfirmedHandler = (difficulty) => {
     setDifficulty(difficulty);
@@ -127,7 +53,7 @@ function Game() {
     if (settingsConfirmed) {
       generateBoard();
     }
-  }, [settingsConfirmed]);
+  }, [settingsConfirmed, difficulty]);
 
   useEffect(() => {
     const checkCompletion = () => {
@@ -155,14 +81,14 @@ function Game() {
       {settingsConfirmed && !gameComplete && (
         <div className="container">
           <Board
+            ref={boardRef}
             board={board}
             setBoard={setBoard}
-            selectedCell={selectedCell}
-            setSelected={setSelectedCell}
+            notes={notes}
           />
           <ActionBar
-            onNumClick={setSelectedCellAsNum}
-            onEraseClick={clearSelectedCell}
+            onNumClick={(num) => boardRef.current.setSelectedCellValue(num)}
+            onEraseClick={() => boardRef.current.clearSelectedCell()}
             notes={notes}
             setNotes={setNotes}
           />
